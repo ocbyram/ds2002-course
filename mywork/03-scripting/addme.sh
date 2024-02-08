99f53612993cc8c332591a4af79f9a4d374cc24d
ocbyram <ocb3wv@virginia.edu>
2024-02-08 15:39:57 -0500

    I'm adding the changes from Feb 6 and the beginning of Feb 8

---
+++ mywork/03-scripting/addme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "What is the first number you want me to add? " FIRNUM
+read -p "What is the second number you want me to add? " SECNUM
+SUMOF=$FIRNUM+$SECNUM
+echo "The sum is $SUMOF"
+
---
+++ mywork/03-scripting/readme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is designed to __________
+# Author: Olivia Byram <ocb3wv@virgina.edu>
+# Last Updated: 02/06/2024
+
+read -p "What is your favorite number? " FAVNUM
+echo "Hey $USERNAME I heard your favorite number is $FAVNUM!"
+
+
